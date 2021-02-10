8bc26d7f95cd7a2e6b5fa790926d9ee7f9ee68a4
Abbe98 <albin.post@gmail.com>
2021-02-10 19:45:59 +0100

    only generate RDF/XML
    leave turtle and json-ld to post file storage processes

--- convert.sh
+++ convert.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 mkdir out && mkdir out/concept
-cp -r $(pwd)/concept/. $(pwd)/out/concept/
+
+for dir in $(find concept -type d);
+  do
+  mkdir out/$dir
+done
 
 for file in $(find concept -type f);
   do
   rapper --input=turtle --input-uri=- --output=rdfxml $file > "out/$(echo "$file" | sed "s/ttl/xml/g")"
-  rapper --input=turtle --input-uri=- --output=json $file > "out/$(echo "$file" | sed "s/ttl/jsonld/g")"
 done
