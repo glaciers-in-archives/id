5b8b721774531df691668a9b93d1df857b6c0a68
Abbe98 <albin.post@gmail.com>
2021-02-17 17:08:50 +0100

    swith output format to rdfxml-abbrev

--- convert.sh
+++ convert.sh
@@ -9,5 +9,5 @@ done
 
 for file in $(find concept -type f);
   do
-  rapper --input=turtle --input-uri=- --output=rdfxml $file > "out/$(echo "$file" | sed "s/ttl/xml/g")"
+  rapper --input=turtle --input-uri=- --output=rdfxml-abbrev $file > "out/$(echo "$file" | sed "s/ttl/xml/g")"
 done
